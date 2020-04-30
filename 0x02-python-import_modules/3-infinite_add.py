618274ef552733c1b57c2364bf58ace94ad9e03e
julian <juliancano.dev@gmail.com>
2020-04-30 17:17:44 -0500

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    if (len(sys.argv) == 1):
+        print("{:d}".format(0))
+    else:
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print(sum)
