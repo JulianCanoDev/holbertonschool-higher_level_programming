8fed9293b33d56ee3b85b70a879c864af9ebf325
julian <juliancano.dev@gmail.com>
2020-04-30 22:07:36 -0500

    task 101

---
+++ 0x02-python-import_modules/hidden_4.py
@@ -0,0 +1,16 @@
+# uncompyle6 version 3.5.0
+# Python bytecode 3.4 (3310)
+# Decompiled from: Python 2.7.5 (default, Aug  7 2019, 00:51:29)
+# [GCC 4.8.5 20150623 (Red Hat 4.8.5-39)]
+# Embedded file name: /vagrant/0x02. Python - import & modules/hidden_4.py
+# Compiled at: 2017-01-03 09:06:49
+# Size of source mod 2**32: 144 bytes
+
+def print_holberton():
+    print('holberton')
+
+def print_school():
+    print('school')
+
+def my_secret_santa():
+    return 98
