8257296caf21ffa0c62e446616e27654fd57a0cb
julian <juliancano.dev@gmail.com>
2020-05-09 17:27:46 -0500

    Advanced

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     score = -99999999999999999999999999999999
-    besty = ""
+    better = ""
     if a_dictionary is not None:
         for key, value in a_dictionary.items():
             if value > score:
                 score = value
-                besty = key
-        return besty
+                better = key
+        return better
     else:
         return None
