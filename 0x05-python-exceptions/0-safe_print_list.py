80b920d5421dd0dd49823855c8fe2b9d28664c4e
julian <juliancano.dev@gmail.com>
2020-05-18 18:20:46 -0500

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+    i = 0
+    try:
+        for items in my_list[0:x]:
+            print("{}".format(items), end="")
+            i += 1
+    except IndexError:
+        print("INVALID INDEX")
+    finally:
+        print()
+        return (i)
