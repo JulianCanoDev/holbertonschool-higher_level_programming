70f984f49cd99fd20522684bb1cb97493eb6ee6d
julian <juliancano.dev@gmail.com>
2020-05-19 21:04:31 -0500

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""this module create the class square with size"""
+
+
+class Square:
+    def __init__(self, size):
+        """__size: Private attribute"""
+        self.__size = size
