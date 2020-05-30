52316589b97da52befc3dd90a4dea97370fdf3e4
julian <juliancano.dev@gmail.com>
2020-05-29 20:38:54 -0500

    Queen

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,76 +1,95 @@
 #!/usr/bin/python3
-"""nQueen"""
+""" This script solves the N-Queen problem"""
 
 
-class Nqueen:
-    """ Class nQueen """
-    def __init__(self, number, board):
-        self.number = number
-        self.board = board
-
-    @property
-    def number(self):
-        """ Getter function """
-        return self.__number
-
-    @number.setter
-    def number(self, value):
-        """ Setter function """
-        self.__number = value
-
-    @property
-    def board(self):
-        """ Getter function"""
-        return self.__board
-
-    @board.setter
-    def board(self, value):
-        """ Setter function """
-        self.__board = value
-
-    def print_board(self):
-        """ printing board """
-        for i in range(self.number):
-            for j in range(self.number):
-                print(self.board[i][j], end="")
-            print()
-
-    def verification(self):
-        """ verifying positions """
-        n1 = 0
-        n2 = 0
-        temp = 0
-        count = 0
-
-        self.__board[n1][n2] = 1
-        for row in range(self.__number):
-            for col in range(self.__number):
-                if self.__board[row][col] == 1:
-                    if row == col:
-                        self.__board[row][col] = 1
-                else:
-                    pass
-        self.print_board()
+import sys
 
 
+def check_moves(matrix, position_x, position_y):
+    """
+    This function checks if a position (x, y) has
+    has a 1 horizontal, vertical or diagonal
+    """
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
+            # Si hay un 1 horizontal
+            if (matrix[x][position_y] == 1):
+                return(0)
+            # Si hay un 1 vertical
+            if (matrix[position_x][y] == 1):
+                return(0)
+            # Si hay un 1 diagonal abajo a la derecha
+            try:
+                if (matrix[position_x + x][position_y + x] == 1):
+                    return(0)
+            except IndexError:
+                pass
+            try:
+                if (position_x - x >= 0):
+                    if (matrix[position_x - x][position_y + x] == 1):
+                        return(0)
+            except IndexError:
+                pass
+            try:
+                if (position_x - x >= 0 and position_y - x >= 0):
+                    if (matrix[position_x - x][position_y - x] == 1):
+                        return(0)
+            except IndexError:
+                pass
+            try:
+                if (position_y - x >= 0):
+                    if (matrix[position_x + x][position_y - x] == 1):
+                        return(0)
+            except IndexError:
+                pass
+    return(1)
 
 
+def put_coords(matrix, result):
+    """
+    This function print a pair of coords to result
+    """
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            if (matrix[i][j] == 1):
+                result[i][0] = i
+                result[i][1] = j
+    return result
 
-if __name__ == "__main__":
-    from sys import argv
 
-    if len(argv) is not 2:
+def recursive_chess(matrix, columns):
+    """
+    This function checks for every queen
+    if there's a queen atacking if not print
+    the n queens position
+    """
+    if (columns == n):
+        result = [[0 for x in range(2)] for y in range(n)]
+        print(put_coords(matrix, result))
+        return
+
+    for row in range(n):
+        if (check_moves(matrix, row, columns) == 1):
+            matrix[row][columns] = 1
+            recursive_chess(matrix, columns + 1)
+            matrix[row][columns] = 0
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if type(int(argv[1])) is not int:
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
-        exit(1)
-    num = int(argv[1])
-    if num < 4:
+        sys.exit(1)
+
+    if n < 4:
         print("N must be at least 4")
-        exit(1)
-    matrix = [[0] * num] * num
-    q = Nqueen(num, matrix)
-    q.print_board()
-    print("----")
-    q.verification()
+        sys.exit(1)
+
+    matrix = [[0 for j in range(n)] for i in range(n)]
+    recursive_chess(matrix, 0)
