54c20df62f3b7cfda6385503150a19f58734593f
julian <juliancano.dev@gmail.com>
2020-06-02 18:55:54 -0500

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Inherit from"""
+
+
 def inherits_from(obj, a_class):
     """Return true if is decend and False if no"""
     return isinstance(obj, a_class) and not type(obj) == a_class
