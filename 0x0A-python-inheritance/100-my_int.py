c6ae3435f60aeb1aa193dce86d36d09a3a9daef8
julian <juliancano.dev@gmail.com>
2020-06-03 12:43:01 -0500

    Advanced taskes

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""my_int revel"""
+
+
+class MyInt(int):
+    """MyInt class"""
+    def __ne__(self, other):
+        return super().__eq__(other)
+
+    def __eq__(self, other):
+        return super().__ne__(other)
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""add_attribute method"""
+
+
+def add_attribute(ob, name, value):
+    """add atribute of an object"""
+    if hasattr(ob, name) or not hasattr(ob, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(ob, name, value)
