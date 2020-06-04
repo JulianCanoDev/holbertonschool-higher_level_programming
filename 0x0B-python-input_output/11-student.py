8096e649d40b17c38151b93d576ba9f8e5b88639
julian <juliancano.dev@gmail.com>
2020-06-04 14:36:28 -0500

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Student that defines a student"""
+import json
+
+
+class Student():
+    """class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """that retrieves a dictionary representation of a Student"""
+        return self.__dict__
