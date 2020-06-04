606be7128c8147b75db230a9ebb268b8d9675aab
julian <juliancano.dev@gmail.com>
2020-06-03 20:54:58 -0500

    Task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Read n lines"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Read n lines"""
+    with open(filename, encoding='utf-8') as my_file:
+        count_line = 0
+
+        for line in my_file:
+            count_line += 1
+            if (nb_lines <= 0) or (nb_lines >= count_line):
+                print(line, end='')
+    my_file.close()
