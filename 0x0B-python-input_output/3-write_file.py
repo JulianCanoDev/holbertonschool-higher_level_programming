e1f603c581be429bafeba18083941dcb54f0b65b
julian <juliancano.dev@gmail.com>
2020-06-03 21:35:18 -0500

    Task 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""Write line"""
+def write_file(filename="", text=""):
+    """Write lines"""
+    with open(filename, mode='w', encoding='utf-8') as my_file:
+        return (my_file.write(text))
