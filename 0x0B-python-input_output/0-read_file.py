59136623c2db122678ae3c953dc61fafc27a6965
julian <juliancano.dev@gmail.com>
2020-06-03 20:00:24 -0500

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Read file function"""
-    with open("my_file_0.txt", mode='r', encoding='utf-8') as my_file:
+    with open(filename, mode='r', encoding='utf-8') as my_file:
         print(my_file.read(), end='')
         my_file.close()
