27b217799a1ad96c051b1e1752e182699ade949b
julian <juliancano.dev@gmail.com>
2020-06-03 19:47:33 -0500

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""I/0 functions"""
+
+
+def read_file(filename=""):
+    """Read file function"""
+    with open("my_file_0.txt", encoding="utf-8") as my_file:
+        print(my_file.read())
