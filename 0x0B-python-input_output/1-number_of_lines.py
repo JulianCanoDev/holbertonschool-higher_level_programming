28d112c25af46d66a2fe68a91d0eae76d7a01aa5
julian <juliancano.dev@gmail.com>
2020-06-03 20:26:37 -0500

    Task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Number of lines"""
+
+
+def number_of_lines(filename=""):
+    """Number of lines"""
+    with open(filename, mode='r', encoding='utf-8') as my_file:
+        count_line = 0
+
+        for line in my_file:
+            count_line += 1
+        return count_line
