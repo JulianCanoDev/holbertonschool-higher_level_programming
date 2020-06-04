b17f06cd263759cd1050dbfaca092e5223f50cef
julian <juliancano.dev@gmail.com>
2020-06-03 22:35:12 -0500

    Task 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Append"""
+
+
+def append_write(filename="", text=""):
+    """Append"""
+    with open(filename, mode='a', encoding='utf-8') as my_file:
+        my_file.write(text)
+        return (len(text))
