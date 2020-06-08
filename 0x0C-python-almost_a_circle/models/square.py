9a1b0b8514c2fb82a92b7e92b5558a8a2b459df7
julian <juliancano.dev@gmail.com>
2020-06-08 12:31:55 -0500

    Squere

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""This is a Square class that heredit from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that heredit from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get-set the size of the Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update Square class"""
+        pass
+
+    def __str__(self):
+        """Return the print() and str()of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
