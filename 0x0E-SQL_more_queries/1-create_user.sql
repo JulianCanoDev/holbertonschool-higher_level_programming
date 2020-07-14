1c7b002b52c32b89ff192f064872a8ca4ea6ed9c
julian <juliancano.dev@gmail.com>
2020-07-14 16:33:12 -0500

    task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
 -- Create user with all privileges and password setted
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT PRIVILEGES
+    ON *.*
+    TO 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
