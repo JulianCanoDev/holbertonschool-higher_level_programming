ef00c4f3325acf01ca12ebbf92f96982dfcd903d
julian <juliancano.dev@gmail.com>
2020-08-18 18:32:38 -0500

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,31 +2,8 @@
 
 
 def find_peak(list_of_integers):
-    """
-    Find a peak in a list of integers.
-    """
-    l = len(list_of_integers)
-    n = l // 2
-    v = 0
-    while v < l:
-        v += 1
-        if n == 0:
-            gtl = True
-        else:
-            gtl = False
-        if n == l - 1:
-            gtr = True
-        else:
-            gtr = False
-        if n > 0:
-            if list_of_integers[n - 1] < list_of_integers[n]:
-                gtl = True
-        if n < l - 1:
-            if list_of_integers[n] > list_of_integers[n + 1]:
-                gtr = True
-        if gtl and gtr:
-            return list_of_integers[n]
-        if not gtr:
-            n += (l - n) // 2
-        elif not gtl:
-            n -= n // 2
+    "find peak in a list"
+    if list_of_integers:
+        return max(list_of_integers)
+    else:
+        return None
