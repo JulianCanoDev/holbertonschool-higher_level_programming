fee6679ba2fa0fcf849f2e807e2908096ca88fd8
julian <juliancano.dev@gmail.com>
2020-08-24 15:06:06 -0500

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST"""
+
+import urllib.parse
+import urllib.request
+import urllib.error
+import sys
+
+try:
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode()
+    with urllib.request.urlopen(sys.argv[1], data=data) as req:
+        print(req.read().decode('utf8'))
+except (urllib.error.URLError, IndexError) as e:
+    print(e)
