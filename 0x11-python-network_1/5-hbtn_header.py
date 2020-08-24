44a9af247308b974998e176d98749ea0f6ed4eba
julian <juliancano.dev@gmail.com>
2020-08-24 18:10:41 -0500

    Task 5 6

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Get value of X-Request-Id header from requested URL"""
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    try:
+        req = get(argv[1])
+        print(req.headers.get('X-Request-Id'))
+    except Exception as err:
+        print(err)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Post an email address to the requested URL"""
+
+if __name__ == "__main__":
+    from requests import post
+    from sys import argv
+
+    try:
+        req = post(argv[1], data={'email': argv[2]})
+        print(req.text)
+    except Exception as e:
+        print(e)
