f7f6e6bd4284f471c5a7cc2ab0d565c4e4e1471c
julian <juliancano.dev@gmail.com>
2020-08-24 18:46:13 -0500

    Task 7 8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Get GitHub id using github API along with Basic Authentication"""
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    try:
+        req = get('https://api.github.com/user', auth=(argv[1], argv[2]))
+        j = req.json()
+        print(j.get('id'))
+    except ValueError as err:
+        print(err)
