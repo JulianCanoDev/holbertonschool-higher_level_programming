2cc5d55ca24c74a2c474fbce07c38a387dc56e6c
julian <juliancano.dev@gmail.com>
2020-08-24 14:52:35 -0500

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as req:
+            print(req.getheader('X-Request-Id'))
+    except (urllib.error.URLError, IndexError) as err:
+        print(err)
