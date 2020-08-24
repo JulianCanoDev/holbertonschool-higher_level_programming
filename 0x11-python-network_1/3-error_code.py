f60f416fecc44390ccef216e12d94627ffbcbda1
julian <juliancano.dev@gmail.com>
2020-08-24 18:03:00 -0500

    Task 3 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Script that sends URL request and prints status code on error"""
+
+
+if __name__ == "__main__":
+    import urllib.error
+    import urllib.request
+    import sys
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as req:
+            print(req.read().decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Fetch URL using requests package"""
+
+if __name__ == "__main__":
+    from requests import get
+
+    try:
+        req = get('https://intranet.hbtn.io/status')
+        print('Body response:')
+        print('\t- type: {}'.format(type(req.text)))
+        print('\t- content: {}'.format(req.text))
+    except Exception as err:
+        print(err)
