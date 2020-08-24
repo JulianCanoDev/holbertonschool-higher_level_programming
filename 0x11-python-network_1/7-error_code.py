6d102cbacb974bdfb500d6da3aa2692555e1d9fb
julian <juliancano.dev@gmail.com>
2020-08-24 18:38:12 -0500

    Task 7 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""print content or status code on error"""
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    try:
+        req = get(argv[1])
+        if req.status_code >= 400:
+            print('Error code: {}'.format(req.status_code))
+        else:
+            print(req.text)
+    except Exception as err:
+        print(err)
