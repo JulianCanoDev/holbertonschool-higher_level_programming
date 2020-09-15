34e6280dc2c3552f87af6792b47d1ee95e737f8e
julian <juliancano.dev@gmail.com>
2020-09-15 16:26:38 -0500

    JavaScript beggin

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 // Count arguments
 const myArgs = process.argv.lengh;
-if (myArgs === 2)
-{
-    console.log('No argument');
-}
-else if (myArgs === 3)
-{
-    console.log('Argument found');
-}
-else
-{
-    console.log('Arguments found');
+if (myArgs === 2) {
+  console.log('No argument');
+} else if (myArgs === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
