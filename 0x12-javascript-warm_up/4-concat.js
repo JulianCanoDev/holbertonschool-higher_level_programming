82a5941212b4c9b32fb12cc4d05a0ae99d0e235d
julian <juliancano.dev@gmail.com>
2020-09-15 20:20:21 -0500

    Task 5

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // Concat
-let 
+console.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Number
+const NUM = parseInt(process.arg[2]);
+if (Number.isInteger(NUM)) {
+    console.log('My number: ' + NUM);
+} else {
+    console.log('Not a number');
+}
