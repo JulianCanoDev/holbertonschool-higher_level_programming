76404fcad933a0cf1f3c5d3863fb68976575c540
julian <juliancano.dev@gmail.com>
2020-09-16 18:04:49 -0500

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const size = parseInt(process.argv[2]);
+const str = 'X';
+if (Number.isInteger(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log(str.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
