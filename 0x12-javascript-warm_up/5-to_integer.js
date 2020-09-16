1db6753820870c50307ebaa1a82875997b9703c8
julian <juliancano.dev@gmail.com>
2020-09-15 23:27:30 -0500

    Task 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // Number
 const num = parseInt(process.argv[2]);
 if (Number.isInteger(num)) {
-    console.log('My number: ' + num);
+  console.log('My number: ' + num);
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
