1c4b0f93c2e995834d5b8540027a28355811f95c
julian <juliancano.dev@gmail.com>
2020-09-15 23:55:46 -0500

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// add
+function add (a, b) {
+  console.log(a + b);
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (Number.isInteger(num1) && Number.isInteger(num2)) {
+  add(num1, num2);
+} else {
+  console.log('NaN');
+}
