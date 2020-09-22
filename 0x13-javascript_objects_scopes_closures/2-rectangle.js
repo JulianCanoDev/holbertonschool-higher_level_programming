946ca7f13ff23f12954cf2278923ae72f28cb1a1
julian <juliancano.dev@gmail.com>
2020-09-21 21:51:47 -0500

    Task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// Rectangle class
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
