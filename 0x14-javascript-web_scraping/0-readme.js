576c21770d6196ff35b983c81584bd282cb1b6bb
julian <juliancano.dev@gmail.com>
2020-09-22 19:06:43 -0500

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 
 try {
-  const data = fs.readFileSync(process.argv[1], 'utf8');
+  const data = fs.readFileSync(process.argv[2], 'utf8');
   console.log(data.toString());
 } catch (e) {
   console.log('Error:', e.stack);
