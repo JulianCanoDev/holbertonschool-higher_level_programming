f69c30759bae22afd5d78afcc23b07fd59644483
julian <juliancano.dev@gmail.com>
2020-09-22 21:43:07 -0500

    Task some

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// gets the contents of a webpage and stores it in a file.
+// gets the contents of a webpage and stores.
 const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
